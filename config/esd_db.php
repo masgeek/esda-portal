4fb68bd081d7c4203b906b1e21f69be233133dd7
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-03 13:43:52 +0300

    db config updates

--- config/esd_db.php
+++ config/esd_db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=esda',
-    'username' => 'esda',
-    'password' => 'esda',
+    'dsn' => 'mysql:host=localhost;dbname=ayes',
+    'username' => 'ayesuser',
+    'password' => 'pdacUZZeRfCCcQrw',
     'charset' => 'utf8',
 ];
