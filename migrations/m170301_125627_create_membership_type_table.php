7938661958af89c1179351da56019702a57fa20a
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-06 15:56:53 +0300

    Added membership table

---
+++ migrations/m170301_125627_create_membership_type_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `membership_type`.
+ */
+class m170301_125627_create_membership_type_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('membership_type', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('membership_type');
+    }
+}
