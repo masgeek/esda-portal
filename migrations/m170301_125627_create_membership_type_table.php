360d798826791d73ab2f5ee3608061e06334595b
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-06 16:14:55 +0300

    Label corrections

--- migrations/m170301_125627_create_membership_type_table.php
+++ migrations/m170301_125627_create_membership_type_table.php
@@ -20,7 +20,7 @@ class m170301_125627_create_membership_type_table extends Migration
         //add teh default data
         $this->insert('membership_type', ['MEMBERSHIP_NAME' =>'RESEARCHER']);
         $this->insert('membership_type', ['MEMBERSHIP_NAME' =>'ENTREPRENEUR']);
-        $this->insert('membership_type', ['MEMBERSHIP_NAME' =>'INCUBATORS']);
+        $this->insert('membership_type', ['MEMBERSHIP_NAME' =>'INCUBATOR']);
         $this->insert('membership_type', ['MEMBERSHIP_NAME' =>'RESOURCE PERSONNEL']);
         $this->insert('membership_type', ['MEMBERSHIP_NAME' =>'MENTOR']);
         $this->insert('membership_type', ['MEMBERSHIP_NAME' =>'OTHER']);
