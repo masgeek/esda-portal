d7d3423076c3a041074d29b6bc4c1941c105f1bd
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-06 15:20:21 +0300

    switched back to english

--- config/web.php
+++ config/web.php
@@ -7,7 +7,7 @@ $config = [
     'name' => YII_DEBUG ? 'ESDA PORTAL <span class="label label-danger">DEV</span>' : 'ESDA PORTAL',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'language' => 'sw', //allows for translation,
+    'language' => 'en', //allows for translation,
     'aliases' => [
         '@bower' => 'vendor/bower-asset',
     ],
