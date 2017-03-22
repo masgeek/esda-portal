e6bcebbd2467ed2b04b630853206283bef8d4bb6
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-22 09:48:24 +0300

    Poertal index page update

--- views/site/index.php
+++ views/site/index.php
@@ -5,65 +5,65 @@
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1><?= Yii::t('app', 'Welcome!') ?></h1>
+        <h1><?= Yii::t('app', 'Welcome to AYES Portal!') ?></h1>
 
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-            labore
-            et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-            dolore eu
-            fugiat nulla pariatur.</p>
+        <p class="lead">AFRICA YOUTH ENTREPRENUERSHIP SOCIETY (AYES) FOR SUSTAINABLE INDUSTRIALIZATION IN AFRICA
+            An Online Platform for new knowledge creation and sharing in support of enterprise development.
+            The Youth Entrepreneurship Society (YES) is a network of young and vibrant African Entrepreneurs and
+            Researchers aged below 40 years, who have the passion and motivation to make a big impact through
+            entrepreneurship development for sustainable industrialization in Africa. This collaboration aims at
+            stimulating new enterprise development by jointly promoting new knowledge creation and sharing through
+            analysis of enterprise-level data and information for solving technical and managerial problems, making
+            projections of future business opportunities, creating a common database for sustainable enterprise
+            development, and generating a new stream of entrepreneurial corps in support of enterprise development.
+            This concept note outlines the design of an online and mobile enabled collaborative platform for the
+            network.
+
+            .</p>
 
         <p><?= Yii::$app->user->isGuest ?
                 \yii\helpers\Html::a(Yii::t('app', Yii::t('app', 'Register')), ['//register'], ['class' => 'btn btn-warning btn-block'])
                 :
-                \yii\helpers\Html::a(Yii::t('app', Yii::t('app', 'Public Documents')), ['//public-docs'], ['class' => 'btn btn-success']) ?></p>
+                \yii\helpers\Html::a(Yii::t('app', Yii::t('app', 'AYES Portal')), ['//public-docs'], ['class' => 'btn btn-success']) ?></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2><?= Yii::t('app', 'Public Documents') ?></h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><?= \yii\helpers\Html::a(Yii::t('app', 'Public Documents'), ['//public-docs'], ['class' => 'btn btn-primary']) ?></p>
-            </div>
-            <div class="col-lg-4">
-                <h2><?= Yii::t('app', 'My Documents') ?></h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><?= \yii\helpers\Html::a(Yii::t('app', Yii::t('app', 'My Documents')), ['//my-uploads'], ['class' => 'btn btn-info']) ?></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu
-                    fugiat nulla pariatur.</p>
+                <h2><?= Yii::t('app', 'Collaboration') ?></h2>
 
                 <p>
-                <p><?= \yii\helpers\Html::a(Yii::t('app', Yii::t('app', 'Public Documents')), ['//my-uploads'], ['class' => 'btn btn-primary']) ?></p></p>
+                    a) Allow African entrepreneurs and researchers to collaborate amongst themselves
+                    b) Allow incubators to collaborate amongst themselves.
+                    c) Allow entrepreneurs to collaborate on joint projects/initiatives.
+                    d) Allow researcher to collaborate on research.
+                    e) Allow researchers and entrepreneurs to share data..
+                    .</p>
+
+                <p><?= \yii\helpers\Html::a(Yii::t('app', 'Collaboration'), ['//public-docs'], ['class' => 'btn btn-primary']) ?></p>
+            </div>
+            <div class="col-lg-4">
+                <h2><?= Yii::t('app', 'Mentorship') ?></h2>
+
+                <p>
+                    a) Incorporate a Mentorship Portal.
+                    b) Allow young entrepreneurs to find and follow mentors on the platform
+                    c) Allow young researchers to find and follow mentors on the platform
+                    .</p>
+
+                <p><?= \yii\helpers\Html::a(Yii::t('app', Yii::t('app', 'Mentorship')), ['//my-uploads'], ['class' => 'btn btn-info']) ?></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Publishing</h2>
+
+                <p>
+                    a) Allow the publishing of Relevant Articles from Researchers and Entrepreneurs
+                    b) Allow Entrepreneurs and Researchers to access relevant articles.
+                </p>
+
+                <p>
+                <p><?= \yii\helpers\Html::a(Yii::t('app', Yii::t('app', 'Publishing')), ['//my-uploads'], ['class' => 'btn btn-primary']) ?></p></p>
             </div>
         </div>
 
