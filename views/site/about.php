0d17de968f3d49524ec5b9374909a2971799405e
Sammy Barasa <barsamms@gmail.com>
2017-07-09 22:53:40 +0300

    added about page information

--- views/site/about.php
+++ views/site/about.php
@@ -4,15 +4,21 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'About AYES';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+    <p>AFRICA YOUTH ENTREPRENEURSHIP SOCIETY (AYES) FOR SUSTAINABLE INDUSTRIALIZATION IN AFRICA
+        An Online Platform for new knowledge creation and sharing in support of enterprise development.
+        The Youth Entrepreneurship Society (YES) is a network of young and vibrant African Entrepreneurs and
+        Researchers aged below 40 years, who have the passion and motivation to make a big impact through
+        entrepreneurship development for sustainable industrialization in Africa. This collaboration aims at
+        stimulating new enterprise development by jointly promoting new knowledge creation and sharing through
+        analysis of enterprise-level data and information for solving technical and managerial problems, making
+        projections of future business opportunities, creating a common database for sustainable enterprise
+        development, and generating a new stream of entrepreneurial corps in support of enterprise development.
+        This concept note outlines the design of an online and mobile enabled collaborative platform for the
+        network.</p>
 </div>
